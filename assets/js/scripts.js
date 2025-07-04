e075d1c2b1a9b64bf18a33c3a447fa5fafe8d54d
herrera1010 <herrera1010@gmail.com>
2025-07-03 19:15:58 -0600

    cambio de pagina

--- assets/js/scripts.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('js-navbar-toggle').addEventListener('click', function() {
-    var menu = document.getElementById('js-menu');
-    menu.classList.toggle('active');
-});
---
+++ script.js
@@ -0,0 +1,5 @@
+function sendEmail(event) {
+    event.preventDefault();
+    alert("Gracias por contactarnos. Te responderemos pronto.");
+    // Aquí puedes integrar un servicio como EmailJS si deseas.
+}
