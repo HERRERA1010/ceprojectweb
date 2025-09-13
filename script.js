fefd0296656852cdb8aa3ea975288ad79a7aad83
herrera1010 <herrera1010@gmail.com>
2025-09-12 20:20:26 -0600

    cambios

--- script.js
+++
@@ -1,5 +0,0 @@
-function sendEmail(event) {
-    event.preventDefault();
-    alert("Gracias por contactarnos. Te responderemos pronto.");
-    // Aquí puedes integrar un servicio como EmailJS si deseas.
-}
